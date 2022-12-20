3fbf94f4be56ea1c8b4396d95833f927166096ac
Dan Garro <dobetterwithdan@gmail.com>
2022-12-20 10:44:57 -0500

    express session part 1

--- app.js
+++ app.js
@@ -9,10 +9,23 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(cookieParser('12345-67890-09876-54321'));
+const session = require('express-session'); 
+const FileStore = require('session-file-store')(session); 
+
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+
+  if (!req.session.user) {
       const authHeader = req.headers.authorization;
       if (!authHeader) {
           const err = new Error('You are not authenticated!');
@@ -25,7 +38,7 @@ function auth(req, res, next) {
       const user = auth[0];
       const pass = auth[1];
       if (user === 'admin' && pass === 'password') {
-          res.cookie('user', 'admin', {signed: true});
+          req.session.user = 'admin';
           return next(); // authorized
       } else {
           const err = new Error('You are not authenticated!');
@@ -34,7 +47,7 @@ function auth(req, res, next) {
           return next(err);
       }
   } else {
-      if (req.signedCookies.user === 'admin') {
+      if (req.session.user === 'admin') {
           return next();
       } else {
           const err = new Error('You are not authenticated!');
