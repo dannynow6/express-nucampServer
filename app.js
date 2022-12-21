f6ea5ad293915fafe9bb70882cc526e256668a13
Dan Garro <dobetterwithdan@gmail.com>
2022-12-21 14:21:07 -0500

    passport

--- app.js
+++ app.js
@@ -4,47 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
 const session = require('express-session'); 
 const FileStore = require('session-file-store')(session); 
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
-// app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter); 
-
-function auth(req, res, next) {
-    console.log(req.session);
-
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
-    }
-}
-
-app.use(auth); 
-
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter'); 
@@ -63,6 +29,7 @@ connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -70,8 +37,40 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter); 
+
+function auth(req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
+    } else {
+        return next(); 
+    }
+}
+
+app.use(auth); 
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(cookieParser());
 
 app.use('/campsites', campsiteRouter); 
 app.use('/promotions', promotionRouter); 
